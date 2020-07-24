1c82fb697b474c9f11d9311fe8af8699a546ef3d
ys <pys99pys@gmail.com>
2020-07-24 16:43:03 +0900

    Add className property in Section

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import Headline from "../Headline";
 
 interface IProps {
+  className?: string;
   title: string;
 }
 
-const Section: React.FC<IProps> = ({ title, children }) => (
-  <section className="section">
+const Section: React.FC<IProps> = ({ className, title, children }) => (
+  <section className={className}>
     <Headline prioty={3}>{title}</Headline>
     {children}
   </section>
